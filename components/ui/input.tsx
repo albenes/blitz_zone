c2eb8a06ecfa0bb82d73ebaa669b65e6bdcc8116
albenes <mendietap@icloud.com>
2024-09-24 22:50:30 +0200

    input.tsx not needed for now

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+/* import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
@@ -21,4 +21,4 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 )
 Input.displayName = "Input"
 
-export { Input }
+export { Input } */
